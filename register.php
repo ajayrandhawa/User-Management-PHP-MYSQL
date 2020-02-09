16927ce2c303f7e036c9f50c51ec1e129ac20f3d
Abhas Dudeja <amdudeja@gmail.com>
2020-02-09 18:16:37 +0530

    Updated Password Function as per Index

--- register.php
+++ register.php
@@ -11,7 +11,7 @@ $final_file=str_replace(' ','-',$new_file_name);
 
 $name=$_POST['name'];
 $email=$_POST['email'];
-$password=md5($_POST['password']);
+$password=password_hash($_POST['password'],PASSWORD_BCRYPT);
 $gender=$_POST['gender'];
 $mobileno=$_POST['mobileno'];
 $designation=$_POST['designation'];
